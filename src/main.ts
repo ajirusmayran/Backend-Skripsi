36146923796429a55f989f7383d5586052e9f5c3
ajirusmayran <aji.rusmayran@quadras.co.id>
2022-07-31 15:55:51 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import configuration from "./config/configuration"
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors()
-	await app.listen(3001)
+	await app.listen(configuration().port)
 }
 bootstrap()
