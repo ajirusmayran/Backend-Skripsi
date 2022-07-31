de60263ae509e870cf8b01c9c6d9bf4b665bdfe7
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2022-07-31 13:06:44 +0700

    Setup Heroku

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common"
 import { UsersService } from "src/users/users.service"
 import * as bcrypt from "bcrypt"
 import { JwtService } from "@nestjs/jwt"
+import UserType from "src/users/const/user.type"
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,20 @@ export class AuthService {
 		private userService: UsersService,
 		private jwtService: JwtService
 	) {}
-	async validateUser(username: string, pass: string): Promise<any> {
+	async validateUser(
+		username: string,
+		pass: string,
+		isAdmin: boolean
+	): Promise<any> {
 		const user = await this.userService.findByUsername(username)
+		if (!user) {
+			return null
+		}
+		if (isAdmin) {
+			if (user.type.toLowerCase() !== UserType.ADMIN) return null
+		}
 		const isMatch = await bcrypt.compare(pass, user.password)
-		if (user && isMatch) {
+		if (isMatch) {
 			const { password, ...result } = user
 			return result
 		}
